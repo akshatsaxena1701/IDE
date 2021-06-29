92c488799eadd953bf427d0df97726bc78652fd6
Akshat Saxena <2019kucp1067@iiitkota.ac.in>
2021-06-30 00:55:12 +0530

    finalver

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var request = require('request');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'));
+app.use(express.json());
+var cors = require('cors')
+
+app.use(cors()) ;
 
 app.get("/",(req,res)=>{
     res.render('home');
@@ -13,13 +17,14 @@ app.get("/",(req,res)=>{
 
 app.post("/code",(req,res)=>{
     
+console.log(req.body);
 
 var program = {
     script : req.body.script,
     language: req.body.language,
     versionIndex: "0",
-    clientId: "",
-    clientSecret:""
+    clientId: "7e753aac2d55bdbfb4ef175c3cb1be26",
+    clientSecret:"a91fdcf8ac8edec672e750550e381b8382f2f3625cea272947f311fba818818d"
 };
 console.log(program);
 request({
